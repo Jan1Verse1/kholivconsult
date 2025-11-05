c887a5210a77cad0a76683ef09e011966cae2791
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2025-11-05 23:44:07 +0100

    change file naming

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import LandingPage from './pages/LandingPage.jsx'
 import ContactPage from './pages/contactUs.jsx'
 import AboutPage from './pages/aboutUs.jsx';
-import PurposePage from './pages/purpose.jsx';
+import PurposePage from './pages/OurPurpose.jsx';
 import { createHashRouter, RouterProvider } from "react-router-dom";
 import './App.css'
 

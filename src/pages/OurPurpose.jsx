5d5e9a286f3bdf5d478269dc8015ec0e489e8e3c
Jan1Verse1 <oluwaseyiadedotun@gmail.com>
2025-11-16 01:56:35 +0100

    bugg

--- src/pages/OurPurpose.jsx
+++ src/pages/OurPurpose.jsx
@@ -54,6 +54,7 @@ const PurposePage = () => {
       <Navbar />
       <IntroImages
         src={purpose}
+      />
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto px-6 py-10">
         <h2 className="text-2xl md:text-3xl lg:text-5xl font-medium text-gray-800 leading-relaxed text-center md:text-left">
           Enterprise Application - The Kholic Way!
